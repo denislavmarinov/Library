56a3199f2c3433aaad465ef614df3094ddea1068
denislav <58664415+denislavmarinov@users.noreply.github.com>
2020-10-25 13:21:41 +0200

    Update users model and migration
    Add columns:
    *first_name
    *last_name
    *role
    *image
    *logged
    Remove column:
    *name

---
+++ database/migrations/2020_10_25_111250_update_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->after('id');
+            $table->string('last_name')->after('first_name');
+            $table->unsignedBigInteger('role')->after('password');
+            $table->text('image')->after('last_name');
+            $table->boolean('logged')->after('role');
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
