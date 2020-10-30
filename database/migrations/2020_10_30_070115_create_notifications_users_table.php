f486204699e71ed2c4886a3b2ed09df4149cef17
denislav <58664415+denislavmarinov@users.noreply.github.com>
2020-10-30 09:03:40 +0200

    Add notifications_users migration
    Notes: notifications_users is a pivot table.

---
+++ database/migrations/2020_10_30_070115_create_notifications_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('notification_id');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('seen');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications_users');
+    }
+}
